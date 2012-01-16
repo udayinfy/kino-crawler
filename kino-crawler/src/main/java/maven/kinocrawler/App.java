403050b74b893ddc56fb22223a5e41218b9ff71e
m.zakrzewski0 <m.zakrzewski0@localhost>
2012-01-16 13:31:21 +0100

    Commit through NetBeans

---
+++ kino-crawler/src/main/java/maven/kinocrawler/App.java
@@ -0,0 +1,13 @@
+package maven.kinocrawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
