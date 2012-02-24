49ee6d02dfaa5959d2b1e3b8f3b9a8e5fe0a3cb3
Adorian <Adorian@AdorianMiniPC>
2012-02-24 16:27:44 +0100

    Program ??czy si? z stron? internetow? ( podan? w kodzie ) i wypisuje  zawarto?? strony (przepraszam , ?e w mainie , ale w podklasie mia?em problemy , ju? kombinuje nan przeniesieniem  , je?eli macie jakis pomys? to podpowiada? ;p )

--- Crawler/src/main/java/com/mycompany/crawler/Initializer.java
+++ Crawler/src/main/java/com/mycompany/crawler/Initializer.java
@@ -2,15 +2,38 @@ package com.mycompany.crawler;
 
 import java.util.StringTokenizer;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Initializer 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        
-        // Tutaj bedzie wywolywany crawler
+       String newLine;
+       URL url = null;
+       URLConnection urlConn = null;
+       InputStreamReader  inStream = null;
+       BufferedReader buffor = null;
+        try {
+            url = new URL("http://www.wp.pl");
+            urlConn = url.openConnection();
+            inStream = new InputStreamReader(urlConn.getInputStream());
+            buffor= new BufferedReader(inStream);
+            while (true){
+            newLine =buffor.readLine();  
+            if (newLine !=null){
+                System.out.println(newLine); 
+            }
+            else{
+               break;
+            } 
+
+            }
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Initializer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
